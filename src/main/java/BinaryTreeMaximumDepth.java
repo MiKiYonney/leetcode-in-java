cfe5399a9312c7eff7aac8d771cee6a2bbbbee9b
yonney.yang <yonney.yang@dianping.com>
2015-03-30 16:08:05 +0800

    SameTree

--- src/main/java/BinaryTreeMaximumDepth.java
+++ src/main/java/BinaryTreeMaximumDepth.java
@@ -1,12 +1,12 @@
 /**
  * Created by yonney.yang on 2015/3/30.
  */
-public class MaximumDepthOfBinaryTree {
+public class BinaryTreeMaximumDepth {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
-        return Math.max(leftDepth,rightDepth)+1;
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 
 }
---
+++ src/main/java/SameTree.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yonney.yang on 2015/3/30.
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
