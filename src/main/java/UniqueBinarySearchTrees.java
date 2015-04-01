7b47b2097d60f08908f1bc7d8ef553fa1104e344
yonney.yang <yonney.yang@dianping.com>
2015-04-01 09:52:37 +0800

    uniqueBinarySearchTree

---
+++ src/main/java/UniqueBinarySearchTrees.java
@@ -0,0 +1,16 @@
+/**
+ * Created by yonney.yang on 2015/3/31.
+ */
+public class UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        int count[] = new int[n+1];
+        count[0] = 1;
+        count[1] = 1;
+        for(int i= 2; i <= n; i++){
+            for(int j = 0; j< i;j++){
+                count[i] += count[j]*count[i-j-1];
+            }
+        }
+        return count[n];
+    }
+}
