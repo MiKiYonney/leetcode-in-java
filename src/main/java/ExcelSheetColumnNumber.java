dd3f98eeb7f614ac8a08f8de854e1c4cfbb333a9
yonney.yang <yonney.yang@dianping.com>
2015-04-11 16:33:33 +0800

    add ExcelSheetColumnNumber

---
+++ src/main/java/ExcelSheetColumnNumber.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yonney.yang on 2015/4/11.
+ */
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int colNumber = 0;
+        for (int i = 0; i < s.length();i++){
+            colNumber = colNumber * 26 + (int)(s.charAt(i)-64);
+        }
+        return colNumber;
+    }
+}
